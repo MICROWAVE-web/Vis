c9a8e8040be9ab4c3b3663364d90afa6a6ec5ef5
alex <playalexei@yandex.ru>
2021-10-22 16:19:32 +0700

    v1.1 - 19.10.2021

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,6 @@ from .views import Vis, validate_url, waprfile
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin_panel'),
     path('vis/', Vis.as_view(), name='home'),
-    path('down/', waprfile, name='down'),
+    path('download/', waprfile, name='download'),
     path('ajax_validate_url/', validate_url)
 ]
